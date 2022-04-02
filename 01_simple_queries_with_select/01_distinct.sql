d81bffa272c8ffb9764f45f98cabd3624585cedb
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-03 00:12:06 +0300

    added distinct and count example

---
+++ 01_simple_queries_with_select/01_distinct.sql
@@ -0,0 +1,7 @@
+/* Выбрать уникальные значения city и country из таблицы employees */
+SELECT DISTINCT city, country
+FROM employees
+
+/* Выбрать уникальные значения players из таблицы games */
+SELECT DISTINCT players 
+FROM games
---
+++ 01_simple_queries_with_select/02_count.sql
@@ -0,0 +1,7 @@
+/* Посчитать сумму всех значений из таблицы orders */
+SELECT COUNT(*)
+FROM orders 
+
+/* Посчитать количество уникальных названий стран колонки country из таблицы employees */
+SELECT COUNT(DISTINCT country)
+FROM employees
