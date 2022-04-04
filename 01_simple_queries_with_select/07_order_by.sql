8f9d89b9792fc63616ee24354a0b6eae1a906ca3
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-04 20:09:56 +0300

    added the sql examples

--- 01_simple_queries_with_select/07_order_by.sql
+++ 01_simple_queries_with_select/07_order_by.sql
@@ -1,14 +1,14 @@
-/*  */
+/* Выбрать все уникальные страны из таблицы customers, при этом сгрупировать их по возрастанию  */
 SELECT DISTINCT country
 FROM customers
 ORDER BY country ASC
 
-/*  */
+/* Выбрать все уникальные страны из таблицы customers, при этом сгрупировать их по убыванию */
 SELECT DISTINCT country
 FROM customers
 ORDER BY country DESC
 
-/*  */
+/* Выбрать все уникальные страны из таблицы customers, при этом сгрупировать их по убыванию, а после в каждом стране сгрупировать city по убыванию */
 SELECT DISTINCT country,city 
 FROM customers
 ORDER BY country DESC, city DESC
---
+++ 01_simple_queries_with_select/08_min_max_svg.sql
@@ -0,0 +1,20 @@
+/* Выбрать колонки ship_city и даты этих заказов order_date з таблицы orders, где ship_city является Лондон и сгрупировать по возрастанию по дате */
+SELECT ship_city, order_date
+FROM orders
+WHERE ship_city = 'London'
+ORDER BY order_date
+
+/* Выбрать самую позднюю дату заказа из таблицы orders, где ship_сity является London */
+SELECT MIN(order_date)
+FROM orders
+WHERE ship_city = 'London'
+
+/* Выбрать самую ранюю дату заказа из таблицы orders, где ship_сity является London */
+SELECT MAX(order_date)
+FROM orders
+WHERE ship_city = 'London'
+
+/* Выбрать среднюю цену товаров з таблицы товаров products, из товаров, которые не имеют скидок */
+SELECT AVG(unit_price)
+FROM products
+WHERE discounted <> 1
