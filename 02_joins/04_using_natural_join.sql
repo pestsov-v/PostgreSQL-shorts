f104cf467884f11665df5b6c180cce7065d3af92
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-06 14:36:28 +0300

    added some joines description

--- 02_joins/04_using_natural_join.sql
+++ 02_joins/04_using_natural_join.sql
@@ -1,4 +1,11 @@
-/*  */
+/* Выбрать столбцы contanct_name, company_name, phone, first_name, last_name, title,
+order_date, product_name, ship_country, product.unit_price, quantity, discount из таблицы orders. 
+При этом внутренним соединением связать данные 
+из order_details у которого внешний ключ order_id соотносится к primary key order_id,
+из products у которого внешний ключ product_id соотносится к primary key product_id,
+из customers у которого внешний ключ customer_id соотносится к primary key customer_id,
+из employees у которого внешний ключ employee_id соотносится к primary key employee_id
+и в результирующий набор попадают только те строки, где ship_country = 'USA' */
 SELECT contanct_name, company_name, phone, first_name, last_name, title,
        order_date, product_name, ship_country, product.unit_price, quantity, discount
 FROM orders
@@ -8,7 +15,7 @@ JOIN customers ON orders.customer_id = customers.customer_id
 JOIN employees ON orders.employee_id = employees.employee_id
 WHERE ship_country = 'USA'
 
-/*  */
+/* Аналогично верхнему запросу. Разница лишь в использовани USING, если название столбцов одноимённые */
 SELECT contanct_name, company_name, phone, first_name, last_name, title,
        order_date, product_name, ship_country, product.unit_price, quantity, discount
 FROM orders
@@ -18,7 +25,9 @@ JOIN customers ON USING(customer_id)
 JOIN employees ON USING(employee_id)
 WHERE ship_country = 'USA'
 
-/*  */
+/* Выбрать столбцы order_id, customer_id, first_name, last_name, title 
+из таблицы orders при этом объеденить внутренним соединением JOIN 
+(или INNER JOIN) с таблицею employees по одноимённым названиям столбцов */
 SELECT order_id, customer_id, first_name, last_name, title
 FROM orders
 NATURAL JOIN employees
--- 02_joins/05_as.sql
+++ 02_joins/05_as.sql
@@ -1,9 +1,19 @@
+/* Посчитать количество строк в таблице employees и 
+подписать результирующий столбец employees_count */
 SELECT COUNT(*) AS employees_count
 FROM employees
 
+/* Посчитать количество уникальных строк в столбце country
+из таблицы employees и подписать результирующий столбец country */
 SELECT COUNT(DISTINCT country) AS country
 FROM employees
 
+/* Выбрать столбец category_id, и подсчитать сумму 
+каждой из категории - units_in_stock. При этом столбец из 
+подсчитанной суммы units_in_stock подписать units_in_Stock.
+Сгрупировать по категориям. 
+Отсортировать по итоговой стоимости units_in_stock по убыванию
+И ограничить результирующий выбор 5 элемента */
 SELECT category_id, SUM(units_in_stock) AS units_in_Stock
 FROM products
 GROUP BY category_id
